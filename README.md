ecb3b14d3736f0dd7aac9cdb1a6cd275645e1464
raulFuzita <raul_fuzita@hotmail.com>
2021-04-20 15:56:30 +0100

    Simple example on how to upload and load a file. Image and user info are stored in a database

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ Every dommy project will at least comes with express, morgan, and nodemon.
 The node_modules and .env file is ignored in this repository. If a project needs
 an .env file you'll see a file called .env.example. If the example you're using
 doesn't despend on .env file you don't have to worry about.
+
+# Important
+
+This example depends on local MongoDB connection. Before you run the code, create 
+a database called `dommyexample` or go to the directory database/mongodb_connector.js 
+and change the name of the database.
--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
+const db = require('./database/mongodb_connector')
+const User = require('./database/user_schema')
 
 const app = express()
+const root = path.join(__dirname+'/views/')
+const upload = require('./middleware/multer_handler')
+app.use(express.static(root))
+app.use(express.static('storage'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use(morgan('dev'))
 
-// Setting up Middleware
-app.use((req, res, next) => {
-    res.locals.path = req.path
-    next()
+app.use('/index', (req, res) => res.sendFile('index.html', {root}))
+app.use('/profile', (req, res) => res.sendFile('profile.html', {root}))
+
+app.use('/update', upload.single('image'), (req, res) => {
+
+    let user = new User()
+    user.name = req.body.name
+    user.file = req.file.filename
+
+    user.save(err => {
+        if (err)
+            return res.sendStatus(400);
+        res.sendFile('profile.html', {root})
+    })
 })
 
+app.use('/search', (req, res) => {
+    const {name} = req.body
+    User.findOne({name}, (err, user) => {
+        if (err) res.json(err)
+        else res.json(user ? user.file : '')
+    })
+})
 
+app.get('/', (req, res) => res.redirect('/index'))
+
+db.conn() // open database connection
 const port = 8000
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
